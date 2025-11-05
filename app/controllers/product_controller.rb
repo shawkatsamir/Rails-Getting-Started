d09b31ceb80818ecfa80056ee813cc9f225f8751
Shawket Ssamir <shawketsamir365@gmail.com>
2025-11-06 00:12:26 +0200

    adding authentication and product caching

--- app/controllers/product_controller.rb
+++ app/controllers/product_controller.rb
@@ -1,4 +1,53 @@
 class ProductController < ApplicationController
+  allow_unauthenticated_access only: %i[ index show]
+
   def index
+    @products = Product.all
   end
+
+  def show
+    @product = Products.find(params[:id])
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to @product
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @product = Products.find(params[:id])
+  end
+
+  def update
+    @product = Products.find(params[:id])
+    if @product.update(product_params)
+      redirect_to @product
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @product.destroy
+    redirect_to products_path
+  end
+
+
+  private
+    def set_product
+      @product = Product.find(params[:id])
+    end
+
+  private
+    def product_params
+      parmas.expect(product: [ :name ])
+    end
 end
